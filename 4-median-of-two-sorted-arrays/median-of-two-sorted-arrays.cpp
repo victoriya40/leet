b8a9ba817b60367c8a18e7a9dc2bee5fc012abcc
victoriya40 <victoriyakrishnan@gmail.com>
2025-07-14 18:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (59.73%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Ensure nums1 is the smaller array
         if (nums1.size() > nums2.size()) {
             return findMedianSortedArrays(nums2, nums1);
         }
-
         int m = nums1.size();
         int n = nums2.size();
         int totalLeft = (m + n + 1) / 2;
-
         int left = 0, right = m;
-
         while (left <= right) {
             int i = (left + right) / 2;
             int j = totalLeft - i;
-
             int nums1LeftMax = (i == 0) ? INT_MIN : nums1[i - 1];
             int nums1RightMin = (i == m) ? INT_MAX : nums1[i];
             int nums2LeftMax = (j == 0) ? INT_MIN : nums2[j - 1];
             int nums2RightMin = (j == n) ? INT_MAX : nums2[j];
-
-            if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
+   if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
                 if ((m + n) % 2 == 0) {
                     return (max(nums1LeftMax, nums2LeftMax) + min(nums1RightMin, nums2RightMin)) / 2.0;
                 } else {
@@ -33,8 +27,6 @@ public:
                 left = i + 1;
             }
         }
-
-        // Control shouldn't reach here
         throw invalid_argument("Input arrays are not sorted or invalid");
     }
 };
