65531d341ab83089b28d448cec2f2a9284d5749d
victoriya40 <victoriyakrishnan@gmail.com>
2026-01-02 14:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.31%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+         int dp [] = new int [n+1];
+         return helper(n , dp);
+        
+    }
+    private int helper( int n , int dp[]){
+        if( n==0 || n==1)return n;
+        if( dp[n] != 0) return dp[n];
+        dp[n] = helper( n-1 , dp) + helper( n-2 , dp);
+        return dp[n];
+    }
+}
