15efe5d9feeffd8eaed2548a8f55ae21f389a48e
victoriya40 <victoriyakrishnan@gmail.com>
2026-02-02 14:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (79.18%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode current=head;
         ListNode previous=null;
@@ -9,21 +9,5 @@
             current=temp;
         }
         return previous;
-    }
-}*/
-class Solution {
-    public ListNode reverseList(ListNode head) {
-
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next; // next node save
-            curr.next = prev;          // link reverse
-            prev = curr;               // prev move
-            curr = next;               // curr move
-        }
-
-        return prev;
     }
 }
